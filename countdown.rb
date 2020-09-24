5ad894e5f29b81b57cc1be1398a4958365e5cb26
Alan Tang <giabao_tang@yahoo.com>
2020-09-24 02:16:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,11 @@ def countdown (seconds)
   puts "HAPPY NEW YEAR!"
 end 
 
-def countdown (seconds)
+def countdown_with_sleep (seconds)
   while seconds > 0 
    puts "#{seconds} SECOND(S)!"
    seconds -=1  
-   sleep 1
+   sleep 5
   end
   puts "HAPPY NEW YEAR!"
 end
